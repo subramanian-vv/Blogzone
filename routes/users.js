b3701faa4994a5ce8b58d7d10efc71632bd2eb0e
Subramanian V V <63257317+subramanian-vv@users.noreply.github.com>
2020-08-08 17:04:10 +0530

    Add edit and delete options

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ const User = require('../models/User');
 //Article model
 const Article = require('../models/Article');
 const { restart } = require('nodemon');
+const e = require('express');
 
 router.get('/feed', ensureAuthenticated, async function (req, res) {
     const articles = await Article.find().sort({ createdDate: 'desc' });
@@ -91,6 +92,41 @@ router.delete('/articles/:id', async function (req, res) {
     await Article.findByIdAndDelete(req.params.id);
     req.flash('error_msg', 'The article has been deleted');
     res.redirect('/user/articles');
-})
+});
+
+router.get('/articles/edit/:id', async function (req, res) {
+    const article = await Article.findById(req.params.id);
+    console.log(article.title);
+    res.render('edit', { 
+        title: req.body.title,
+        description: req.body.description,
+        content: req.body.content,
+        article: article });
+});
+
+router.put('/articles/:id', async function (req, res) {
+    req.article = await Article.findById(req.params.id);
+    
+    let article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.content = req.body.content;
+    try {
+        article = await article.save();
+        console.log("Done");
+        req.flash('success_msg', 'The article has been updated');
+        res.redirect('/user/articles');
+    } 
+    catch (err) {
+        console.log(err);
+        res.render('edit', { 
+            title: req.body.title,
+            description: req.body.description,
+            content: req.body.content,
+            article: article });
+        req.flash('error_msg', 'Please fill all the fields');
+        console.log("ERRORRRRRRRRRRRRRR!!!");
+    }
+});
 
 module.exports = router;
